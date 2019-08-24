1b954bd5d38e2749c5e582a8457b8268879b8afa
kleencut <kleencut@yahoo.com>
2019-08-24 14:00:01 +1000

    add existing

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# bb_materialize_toh
-Augular Tour of Heroes styled with Materialize UI framework
+# Angualr Toh and Materialize
+
+An implementation of the [Augular Tour of Heroes](https://angular.io/tutorial/toh-pt0) styled with [Materialize Css](http://materializecss.com)
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9. 
+
+A short blog discussion also available at [https://kleencut.wordpress.com/2019/08/24/materialize-angular-tour-of-heroes/](https://kleencut.wordpress.com/2019/08/24/materialize-angular-tour-of-heroes/)
+
+
+### To install
+```bash
+$ npm install
+```
+### Run development server
+
+
+```bash
+$ ng serve
+```
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+### Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+### Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from './../hero';
+import { Location } from '@angular/common';
+import { HeroService } from './../hero.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private heroService: HeroService,
+    private location: Location) {}
+
+    ngOnInit(): void {
+      this.getHero();
+    }
+
+    getHero(): void {
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void {
+      this.location.back();
+    }
+
+    save(): void {
+      this.heroService.updateHero(this.hero)
+      .subscribe( () => this.goBack() )
+    }
+
+    delete(): void {
+      this.heroService.deleteHero(this.hero).subscribe();
+      this.router.navigate(['/heroes']);
+    }
+
+}
